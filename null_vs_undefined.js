5a95447bd7ae7eb95c9626b18e21c81e11c67939
mahabubealahirahat <mahabubrahat1@gmail.com>
2020-03-29 23:58:23 +0600

    null_vs_undefined done

---
+++ null_vs_undefined.js
@@ -0,0 +1,18 @@
+let x;
+ console.log(x);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    
+}
+const result = add(22)
+console.log(result);
+
+ let object = {name:"noName", age:"noAge"};
+console.log(object.fullName);
+
+let r = null;
+console.log(r);
+
+
+
